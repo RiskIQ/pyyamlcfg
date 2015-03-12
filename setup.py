41edec803a9a82b0da25aa89975ac2e8338f7cec
Johan Nestaas <johannestaas@gmail.com>
2015-03-12 10:57:53 -0700

    0.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name = "treeconf",
-    version = "0.0.1",
+    version = "0.1.0",
     description = "Hierarchical configuration parser",
     author = "Johan Nestaas",
     author_email = "johan@riskiq.net",
